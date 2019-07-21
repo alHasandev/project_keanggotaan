78b47372c4a7f10ad0c7ec790ec2dd8df8cb1b87
alHasandev <alhasan99dev@gmail.com>
2019-07-21 21:30:45 +0800

    menambahkan style ke tentang

--- index.php
+++ index.php
@@ -10,56 +10,57 @@
 </head>
 
 <body>
-      <div class="container">
-  <header>
-    <h1>DTS Kelompok 7</h1>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php">Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <div class="container">
+    <header>
+      <h1>DTS Kelompok 7</h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link"  href="tentang.php">Tentang Kami <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link"  href="login.php">Login<span class="sr-only">(current)</span></a>
-      </li>
-    
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-  </header>
-  <br>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="tentang.php">Tentang Kami <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="login.php">Login<span class="sr-only">(current)</span></a>
+            </li>
 
-
-   <div class="jumbotron">
-  <h1 class="display-4">  <h2>Kelompok 7 Mempersembahkan</h2></h1>
-  <hr>
-  <p class="lead">
-  <div class="row">
-          <span class="label"> </span>
-          
-          </div>
-    <h1>Sistem Informasi Manajemen Asosiasi Web Programmer Indonesia (SIMAWI)</h1>
-    <p>Ayo bergabung bersama kami di Asosiasi Pejuang Malam</p>
-    <p>Silahkan Login atau Daftar jika anda belum memiliki akun</p>
-    <a href="login.php" class="btn btn-primary">login</a>
-    <a href="anggota/daftar.php" class="btn btn-primary">Daftar</a>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
     <br>
-</div>
-  <footer>
-    <p>DTS Kelompok 7 &copy; 2019</p>
-  </footer>
 
-   <script src="js/jquery-3.3.1.min.js"></script>
-   <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <div class="jumbotron">
+      <h1 class="display-4">
+        <h2>Kelompok 7 Mempersembahkan</h2>
+      </h1>
+      <hr>
+      <p class="lead">
+        <div class="row">
+          <span class="label"> </span>
+
+        </div>
+        <h1>Sistem Informasi Manajemen Asosiasi Web Programmer Indonesia (SIMAWI)</h1>
+        <p>Ayo bergabung bersama kami di Asosiasi Pejuang Malam</p>
+        <p>Silahkan Login atau Daftar jika anda belum memiliki akun</p>
+        <a href="login.php" class="btn btn-primary">login</a>
+        <a href="anggota/daftar.php" class="btn btn-primary">Daftar</a>
+        <br>
+    </div>
+    <footer>
+      <p>DTS Kelompok 7 &copy; 2019</p>
+    </footer>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- tentang.php
+++ tentang.php
@@ -6,38 +6,64 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Tentang Kelompok 7</title>
+  <link rel="stylesheet" href="style/bootstrap.min.css">
 </head>
 
 <body>
-  <header>
-    <h1>DTS Kelompok 7</h1>
-    <nav>
-      <ul class="nav-left">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="tentang.php">Tentang Kami</a></li>
-      </ul>
-      <ul class="nav-right">
-        <li><a href="login.php">Login</a></li>
-      </ul>
-    </nav>
-  </header>
-
   <div class="container">
-    <h2>Kami Adalah Kelompok ke-7</h2>
-    <h3>Dari sesi 1 pelatihan Junior Web Developer Tema Vocational School Graduate Academy</h3>
-    <h3>Anggota Kelompok :</h3>
-    <ul>
-      <li>Al Kaderi</li>
-      <li>Azidan</li>
-      <li>Mohamad Albie</li>
-      <li>Ronaldy</li>
-    </ul>
-    <h1>Digitalent Kominfo 2019</h1>
+    <header>
+      <h1>DTS Kelompok 7</h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="tentang.php">Tentang Kami <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="login.php">Login<span class="sr-only">(current)</span></a>
+            </li>
+
+          </ul>
+
+        </div>
+      </nav>
+    </header>
+    <br>
+    <!-- bagian html header -->
+
+    <div class="jumbotron">
+      <h2>Kami Adalah Kelompok ke-7</h2>
+      <p>Dari sesi 1 pelatihan Junior Web Developer Tema Vocational School Graduate Academy</p>
+      <hr>
+      <h3>Anggota :</h3>
+      <ul>
+        <li>Al Kaderi</li>
+        <li>Azidan</li>
+        <li>Mohamad Albie</li>
+        <li>Ronaldy</li>
+      </ul>
+      <hr>
+      <h3>Pengajar :</h3>
+      <ul>
+        <li>Pa Agus SBN</li>
+        <li>Ibu Fitri</li>
+      </ul>
+      <hr>
+      <h1>Digitalent Kominfo Poliban 2019</h1>
+    </div>
+
+    <footer>
+      <p>DTS Kelompok 7 &copy; 2019</p>
+    </footer>
   </div>
 
-  <footer>
-    <p>DTS Kelompok 7 &copy; 2019</p>
-  </footer>
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
